112b0059b0cecfad5afe8ba50d2dfe72695edf5b
Miguel Luiz <111905998+MiguelLuizDaPe@users.noreply.github.com>
2022-11-26 19:46:24 -0300

    Add files via upload

--- Saque.java
+++ Saque.java
@@ -1,10 +1,10 @@
 public class Saque extends TransacaoBancaria{
-	Efetuante bancoDoCliente;
-	Efetuante bancoDoValorTirado;
+	String bancoDoCliente;
+	String bancoDoValorTirado;
 	Cliente cliente;
 	int valorTirado;
 
-	public Saque(Efetuante bancoDoCliente, Efetuante bancoDoValorTirado, Cliente cliente, int valorTirado){
+	public Saque(String bancoDoCliente, String bancoDoValorTirado, Cliente cliente, int valorTirado){
 		this.bancoDoCliente = bancoDoCliente;
 		this.bancoDoValorTirado = bancoDoValorTirado;
 		this.cliente = cliente;
@@ -12,8 +12,15 @@ public class Saque extends TransacaoBancaria{
 	}
 
 	@Override
+	public void transacao(){
+		int clienteDin = this.cliente.getDinheiro();
+		clienteDin -= this.valorTirado;
+		this.cliente.setDinheiro(clienteDin);
+	}
+
+	@Override
 	public String detalhar(){
-		return "O cliente ("+this.cliente+") do banco "+this.bancoDoCliente+" sacou "+this.valorTirado+
+		return "O cliente ("+this.cliente.getNome()+") do banco "+this.bancoDoCliente+" sacou "+this.valorTirado+
 					 " reais no caixa do banco"+this.bancoDoValorTirado;
 	}
 	
